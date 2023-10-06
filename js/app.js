c5516d8c11bf1a052b0d0e3fae1f85dd210d0710
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-06 18:12:57 +0200

    edit js readme

--- js/app.js
+++ js/app.js
@@ -2,19 +2,17 @@
 
 // -creo il ciclo for 
 //   -numeri da 1 a 100 
-for (let i = 1; i <= 100; i++) { //-quindi scrivo i = 1 < 100; i++ (incrementa)
-console.log(i) //-stampo in console 
+for (let i = 0; i < 100; i++) { //-quindi scrivo i = 1 < 100; i++ (incrementa)
+  const n = i + 1;
 
-// -SE sono multipli di 3 
-if ( i % 3 === 0) {
-  console.log(i, 'buzz');
+
+  if(n % 3 === 0 && n % 5 === 0) { //se è multiplo di tre e cinque 
+  console.log ('fizzbuzz');
+}else if (n % 3 === 0) { //se è multiplo di tre 
+  console.log('fizz');
+}else if (n % 5 === 0) { //se è multiplo di cinque
+  console.log ('buzz');
+}else{                  //se non è multiplo di niente 
+  console.log(n)
 }
 }
-
-
-
-//   -stampo FIZZ 
-// ALTRIMENTI SE sono multipli di 5 
-//   -stampo BUZZ
-// -SE sono multipli di 5 e di 3 stampo FIZZBUZZ 
-
--- readme.md
+++ readme.md
@@ -2,13 +2,16 @@
 
 -creo il ciclo for 
   -numeri da 1 a 100 
-  -quindi scrivo i = 1 < 100; i++ (incrementa)
-  -stampo in console 
+  -quindi scrivo i = 0 < 100; i++ 
+    let n = i + 1 (incrementa)
+   
 
--SE sono multipli di 3 
-  -stampo Fizz   
-ALTRIMENTI SE sono multipli di 5 
+-SE sono multipli di 5 e di 3 stampo FIZZBUZZ  
+  -stampo FizzBUZZ  
+-ALTRIMENTI SE sono multipli di 3 
   -stampo Buzz
--SE sono multipli di 5 e di 3 stampo FIZZBUZZ 
+-ALTRIMENTI SE sono multipli di 5
+-ALTRIMENTI se non sono multipli stampo (n)
 
+-stampo in console
 
